4569859f3c754aa78c4c4ad0aa0f0f3048a59090
MeParas325 <adamchacha398@gmail.com>
2023-05-10 11:50:27 +0530

    Step-3

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          leading: Icon(CupertinoIcons.home),
+          title: Text("Our Chat"),
+          actions: [
+            IconButton(onPressed: () {
+              
+            }, icon: Icon(Icons.search)),
+            IconButton(onPressed: () {
+              
+            }, icon: Icon(Icons.more_vert)),
+          ]),
+
+          floatingActionButton: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FloatingActionButton(onPressed: () {
+              
+
+              
+            }, child:Icon(Icons.add_comment_rounded)),
+          ),
+    );
+  }
+}
